9f258a2600348b0c9227b99e14a3db459f971851
minchan30 <goalie0901@gmail.com>
2023-09-10 12:24:26 +0900

    :memo:update

--- 반복문/2438.py
+++ 반복문/2438.py
@@ -2,7 +2,7 @@ num = int(input())
 # for i in range(1 , num + 1) :
 #     print('*' * i)
 
-for i in range(?):
-    for j in range(?):
+for i in range(1 , num + 1):
+    for j in range(i):
         print("*", end='')
     print()
